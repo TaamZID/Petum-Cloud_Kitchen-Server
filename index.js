b98a37d5630e551b842edf77abbbab099c4f0bc1
TaamZID <tamjidalam00@gmail.com>
2022-11-08 00:25:39 +0600

    dotend mongodb added

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+
 var cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
+
+console.log(process.env.DB_USER);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pgthecp.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 const datas = require("./data/datas.json");
 
